85352361e93f3f209a84b6c5c2a319dcfe6b6d2e
danieletie <109879077+danieletie@users.noreply.github.com>
2025-11-05 21:14:03 +0100

    Update validation on Sign Up form
    - Added javascript file and code to prompt when
      invalid email address entered
    - updated html file to validate password, making
      sure it contains minimum eight characters, at
      least one uppercase letter, one lowercase letter
      one number and one special character.

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+const email = document.getElementById("user_email");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+
